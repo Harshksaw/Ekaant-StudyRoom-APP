394129d27b24717329a3f47994941a513ef2c542
Harsh kumar <mister.harshkumar@gmail.com>
2024-06-16 16:55:38 +0530

    feat: Add lazy loading for images and update login screen UI
    This commit adds lazy loading functionality for images to improve page load performance. It also updates the UI of the login screen by adding a sign-up button and improving the styling of the input fields.

--- studyroom/screens/auth/login/login.screen.tsx
+++ studyroom/screens/auth/login/login.screen.tsx
@@ -1,5 +1,5 @@
 import { router } from "expo-router";
-import React from "react";
+import React, { useState } from "react";
 import {
   Text,
   TextInput,
@@ -9,31 +9,66 @@ import {
   View,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+
+
+
 const LoginScreen: React.FC = () => {
-  const [email, setEmail] = React.useState("");
-  const [password, setPassword] = React.useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleLogin = () => {
     // Implement your login logic here
   };
 
   return (
+    
     <SafeAreaView style={styles.container}>
       <Text>Login</Text>
-      <TextInput placeholder="Email" value={email} onChangeText={setEmail} />
+
+      <View style={styles.inputContainer}>
       <TextInput
+        style={[styles.input, { paddingLeft: 40, marginBottom: -12 }]}
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+        />
+
+<TextInput
+  style={[styles.input, { paddingLeft: 40, marginBottom: -12 }]}
         placeholder="Password"
         secureTextEntry
         value={password}
         onChangeText={setPassword}
       />
-      <Button title="Login" onPress={handleLogin} />
+        </View>
+     
+     <TouchableOpacity
+        style={{
+          padding: 16,
+          borderRadius: 8,
+          marginHorizontal: 16,
+          backgroundColor: "#2467E2",
+          marginTop: 15,
+        }}
+     >
+
+        <Text
+                    style={{
+                      color: "white",
+                      textAlign: "center",
+                      fontSize: 16,
+                      fontFamily: "Raleway_700Bold",
+                    }}
+                    >
+                    Sign Up
+                  </Text>
+                    </TouchableOpacity>
 
       <View style={styles.signupRedirect}>
         <Text style={{ fontSize: 18, fontFamily: "Raleway_600SemiBold" }}>
           Don't have an account?
         </Text>
-        <TouchableOpacity onPress={() => router.push("/(routes)/login")}>
+        <TouchableOpacity onPress={() => router.push("/(routes)/welcome")}>
           <Text
             style={{
               fontSize: 18,
@@ -48,21 +83,61 @@ const LoginScreen: React.FC = () => {
       </View>
     </SafeAreaView>
   );
-};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    // backgroundColor: 'black',
-    // justifyContent: 'center',
-    // alignItems: 'center',
-  },
-  signupRedirect: {
-    flexDirection: "row",
-    marginHorizontal: 16,
-    justifyContent: "center",
-    marginBottom: 20,
-    marginTop: 20,
-  },
-});
-export default LoginScreen;
+}
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      padding: 16,
+    },
+    signupRedirect: {
+      flexDirection: "row",
+      marginTop: 16,
+    },
+    inputContainer: {
+      width: "100%",
+      marginHorizontal: 16,
+      // marginTop: 30,`
+      rowGap: 30,
+    },
+    input: {
+      height: 55,
+      marginHorizontal: 16,
+      borderRadius: 8,
+      paddingLeft: 35,
+      fontSize: 16,
+
+      backgroundColor: "white",
+      color: "#A1A1A1",
+    },
+    visibleIcon: {
+      position: "absolute",
+      right: 30,
+      top: 15,
+    },
+    icon2: {
+      position: "absolute",
+      left: 23,
+      top: 17.8,
+      marginTop: -2,
+    },
+    forgotSection: {
+      marginHorizontal: 16,
+      textAlign: "right",
+      fontSize: 16,
+      marginTop: 10,
+    },
+    signupRedirect: {
+      flexDirection: "row",
+      marginHorizontal: 16,
+      justifyContent: "center",
+      marginBottom: 20,
+      marginTop: 20,
+    },
+  });
+  export default LoginScreen;
+
+
+
