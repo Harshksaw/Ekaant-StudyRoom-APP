085b20ad20c4d0c00f650cb5538f6de5c8423141
Gourish Markan <142171408+GourishMarkan@users.noreply.github.com>
2024-06-29 00:23:03 +0530

    forgotPassowrd

--- Backend/src/routes/v1/auth.routes.js
+++ Backend/src/routes/v1/auth.routes.js
@@ -1,17 +1,14 @@
-const express = require('express');
+const express = require("express");
 
-const { AuthController  } = require('../../controllers');
+const { AuthController } = require("../../controllers");
 const AuthRouter = express.Router();
 
-
-
-AuthRouter.get('/ping', AuthController.pingAuthController);
-AuthRouter.post('/signup', AuthController.signUp);
-AuthRouter.post('/signin', AuthController.signIn);
+AuthRouter.get("/ping", AuthController.pingAuthController);
+AuthRouter.post("/signup", AuthController.signUp);
+AuthRouter.post("/signin", AuthController.signIn);
 // AuthRouter.put('/profile', AuthController.updateProfile);
-AuthRouter.post('/otp', AuthController.generateOtp);
-AuthRouter.post('/verifyOtp', AuthController.verifyOtp);
-
-
+AuthRouter.post("/otp", AuthController.generateOtp);
+AuthRouter.post("/verifyOtp", AuthController.verifyOtp);
+AuthRouter.post("/forgotPassword", AuthController.forgetPassword);
 
 module.exports = AuthRouter;
