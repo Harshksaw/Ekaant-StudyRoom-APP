8d8672f5a1d0abdd591687cadebfdd3945d17482
Harsh kumar <mister.harshkumar@gmail.com>
2024-06-28 18:01:30 +0530

    chore: Add booking model to the project

---
+++ Backend/src/models/booking.model.js
@@ -0,0 +1,71 @@
+const mongoose = require("mongoose");
+
+// Define the library schema
+const bookingSchema = new mongoose.Schema({
+
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "User",
+    },
+    libraryId: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "Library",
+    },
+    initialPrice:{
+        type: Number,
+        required: true,
+
+    },
+    finalPrice:{
+        type: Number,
+        required: true,
+    },
+    paid:{
+        type: Boolean,
+        required: true,
+    },
+    timeSlot:{
+        from: { type: String, required: false },
+        to: { type: String, required: false },
+      },
+    bookedSeat: {
+          id: { type: String, required: false },
+          label: { type: String, required: false },
+        },
+    bookingDate:{
+        type: Date,
+        // default: Date.now,
+        required: true,
+    },
+    bookingPeriod: {
+        type: Number,  
+        default: 1,
+        required: false,
+       
+    },
+    transactionDetails:{
+        type: {},
+        required: false,
+    },
+
+
+    timeStamp: {
+        type: Date,
+        default: Date.now,
+    },
+
+
+
+
+
+
+    
+
+});
+
+
+module.exports = {
+  Library: mongoose.model("Booking", bookingSchema),
+};
