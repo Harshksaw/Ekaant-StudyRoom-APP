97721ec01cbc767f89ad4e4af0e64192de27b715
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2024-07-01 17:15:49 +0000

    chore: Update BASEURL and BACKEND configuration for local development

--- studyroom/utils/config.ts
+++ studyroom/utils/config.ts
@@ -1,2 +1,3 @@
 // export const BACKEND='https://studyroom-app.onrender.com'
-export const BACKEND='https://jubilant-space-disco-gwgvjp5x96wc996q-3000.app.github.dev'
+// export const BACKEND='https://jubilant-space-disco-gwgvjp5x96wc996q-3000.app.github.dev'
+export const BACKEND='http://127.0.0.1:3000'
