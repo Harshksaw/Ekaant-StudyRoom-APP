1b8162b8e04cfc607fa994e691235072757874a8
Harsh kumar <mister.harshkumar@gmail.com>
2024-07-07 22:49:22 +0530

    chore: Refactor Signup component UI and add OTP input fields

---
+++ admin/src/Auth/Signup/Step3.tsx
@@ -0,0 +1,224 @@
+export const StepThree = ({
+  nextStep,
+  prevStep,
+  userDetails,
+  setUserDetails,
+}) => (
+  //images  - Register 3
+  <div className="flex  flex-1 overflow-y-auto px-10 py-6 bg-white rounded-lg">
+    {/* Form fields for step 2 */}
+
+    <div className=" flex-col w-full  mb-100 gap-20">
+      <div className="flex-col items-center justify-start">
+        <label
+          htmlFor="fullName"
+          className="w-1/3 text-gray-700 text-left font-mulish font-bold text-md leading-tight"
+        >
+          Full Name
+        </label>
+        <input  className="w-full px-3 py-2  border border-gray-800 focus:outline-none focus:ring-1 focus:ring-blue-500"  className="w-full px-3 py-2  border border-gray-800 focus:outline-none focus:ring-1 focus:ring-blue-500"  className="w-full px-3 py-2  border border-gray-800 focus:outline-none focus:ring-1 focus:ring-blue-500"
+          type="text"
+          id="fullName"
+          value={userDetails.fullName}
+          onChange={(e) =>
+            setUserDetails({ ...userDetails, fullName: e.target.value })
+          }
+          placeholder="Full Name"
+          
+        />
+      </div>
+      {/* DOB */}
+      <div className="flex-col items-center justify-start">
+        <label
+          htmlFor="dob"
+          className="w-1/3 text-gray-700 text-left font-mulish font-bold text-md leading-tight"
+        >
+          DOB
+        </label>
+        <input  className="w-full px-3 py-2  border border-gray-800 focus:outline-none focus:ring-1 focus:ring-blue-500"  className="w-full px-3 py-2  border border-gray-800 focus:outline-none focus:ring-1 focus:ring-blue-500"  className="w-full px-3 py-2  border border-gray-800 focus:outline-none focus:ring-1 focus:ring-blue-500"
+          type="text"
+          id="dob"
+          value={userDetails.dob}
+          onChange={(e) =>
+            setUserDetails({ ...userDetails, dob: e.target.value })
+          }
+          placeholder="DOB"
+         
+        />
+      </div>
+      {/* Aadhar Card */}
+
+      <div className="flex-col items-center justify-start">
+        <label
+          htmlFor="aadharCard"
+          className="w-1/3 text-gray-700 text-left font-mulish font-bold text-md leading-tight"
+        >
+          Aadhar Card
+        </label>
+        <input  className="w-full px-3 py-2  border border-gray-800 focus:outline-none focus:ring-1 focus:ring-blue-500"  className="w-full px-3 py-2  border border-gray-800 focus:outline-none focus:ring-1 focus:ring-blue-500"  className="w-full px-3 py-2  border border-gray-800 focus:outline-none focus:ring-1 focus:ring-blue-500"
+          type="text"
+          id="aadharCard"
+          value={userDetails.aadharCard}
+          onChange={(e) =>
+            setUserDetails({ ...userDetails, aadharCard: e.target.value })
+          }
+          placeholder="Aadhar Card"
+       
+        />
+      </div>
+      {/* Upload Aadhar */}
+
+      <div className="flex-col   items-center ">
+        <label
+          htmlFor="uploadPanCard"
+          className="w-full text-gray-700 text-left font-mulish font-bold text-md leading-tight"
+        >
+          Addhar Card
+        </label>
+
+        <input  className="bg-blue-400 py-2  border border-gray-800 focus:outline-none focus:ring-1 focus:ring-blue-500"  className="w-full px-3 py-2  border border-gray-800 focus:outline-none focus:ring-1 focus:ring-blue-500"  className="w-full px-3 py-2  border border-gray-800 focus:outline-none focus:ring-1 focus:ring-blue-500"
+          type="file"
+          color="blue"
+          id="uploadAddharCard"
+          accept="image/*" // Restrict file types (optional)
+          onChange={(e) => {
+            const file = e.target.files ? e.target.files[0] : null;
+            if (file) {
+              setUserDetails({
+                ...userDetails,
+                uploadAadharCard: file,
+              });
+            }
+          }}
+         
+        />
+      </div>
+      <div className="flex-col items-center justify-start">
+        <label
+          htmlFor="PanCard"
+          className="w-1/3 text-gray-700 text-left font-mulish font-bold text-md leading-tight"
+        >
+          PAN Card
+        </label>
+        <input  className="w-full px-3 py-2  border border-gray-800 focus:outline-none focus:ring-1 focus:ring-blue-500"  className="w-full px-3 py-2  border border-gray-800 focus:outline-none focus:ring-1 focus:ring-blue-500"  className="w-full px-3 py-2  border border-gray-800 focus:outline-none focus:ring-1 focus:ring-blue-500"
+          type="text"
+          id="PanCard"
+          value={userDetails.aadharCard}
+          onChange={(e) =>
+            setUserDetails({ ...userDetails, aadharCard: e.target.value })
+          }
+          placeholder="Aadhar Card"
+      
+        />
+      </div>
+
+      <div className="flex-col   items-center justify-start">
+        <label
+          htmlFor="uploadPanCard"
+          className="w-full text-gray-700 text-left font-mulish font-bold text-md leading-tight"
+        >
+          Upload Pan Card
+        </label>
+
+        <input  className="w-full px-3 py-2  border border-gray-800 focus:outline-none focus:ring-1 focus:ring-blue-500"  className="w-full px-3 py-2  border border-gray-800 focus:outline-none focus:ring-1 focus:ring-blue-500"  className="w-full px-3 py-2  border border-gray-800 focus:outline-none focus:ring-1 focus:ring-blue-500"
+          type="file"
+          color="blue"
+          id="uploadPanCard"
+          accept="image/*" // Restrict file types (optional)
+          onChange={(e) => {
+            const file = e.target.files ? e.target.files[0] : null;
+            if (file) {
+              setUserDetails({
+                ...userDetails,
+                uploadAadharCard: file,
+              });
+            }
+          }}
+        
+        />
+      </div>
+
+      {/* addresses */}
+
+      <div className="flex flex-col gap-6 ">
+        <label className="text-xl">Address</label>
+        {/* line 1 */}
+        <input  className="w-full px-3 py-2  border border-gray-800 focus:outline-none focus:ring-1 focus:ring-blue-500"  className="w-full px-3 py-2  border border-gray-800 focus:outline-none focus:ring-1 focus:ring-blue-500"  className="w-full px-3 py-2  border border-gray-800 focus:outline-none focus:ring-1 focus:ring-blue-500"
+          type="text"
+         
+          value={userDetails.address.line1}
+          onChange={(e) => {
+            setUserDetails({
+              ...userDetails,
+              address: {
+                ...userDetails.address,
+                line1: e.target.value,
+              },
+            });
+          }}
+          placeholder="Address Line 1"
+        />
+        {/* line 2 */}
+        <input  className="w-full px-3 py-2  border border-gray-800 focus:outline-none focus:ring-1 focus:ring-blue-500"  className="w-full px-3 py-2  border border-gray-800 focus:outline-none focus:ring-1 focus:ring-blue-500"  className="w-full px-3 py-2  border border-gray-800 focus:outline-none focus:ring-1 focus:ring-blue-500"
+          type="text"
+          value={userDetails.address.line2}
+          onChange={(e) => {
+            setUserDetails({
+              ...userDetails,
+              address: {
+                ...userDetails.address,
+                line2: e.target.value,
+              },
+            });
+          }}
+          placeholder="Address Line 2"
+        />
+
+        {/* city */}
+        <input  className="w-full px-3 py-2  border border-gray-800 focus:outline-none focus:ring-1 focus:ring-blue-500"  className="w-full px-3 py-2  border border-gray-800 focus:outline-none focus:ring-1 focus:ring-blue-500"  className="w-full px-3 py-2  border border-gray-800 focus:outline-none focus:ring-1 focus:ring-blue-500"
+          type="text"
+          value={userDetails.address.city}
+          onChange={(e) => {
+            setUserDetails({
+              ...userDetails,
+              address: {
+                ...userDetails.address,
+                city: e.target.value,
+              },
+            });
+          }}
+          placeholder="City"
+        />
+        {/* pinCode */}
+        <input  className="w-full px-3 py-2  border border-gray-800 focus:outline-none focus:ring-1 focus:ring-blue-500"  className="w-full px-3 py-2  border border-gray-800 focus:outline-none focus:ring-1 focus:ring-blue-500"  className="w-full px-3 py-2  border border-gray-800 focus:outline-none focus:ring-1 focus:ring-blue-500"
+          type="text"
+          value={userDetails.address.pincode}
+          onChange={(e) => {
+            setUserDetails({
+              ...userDetails,
+              address: {
+                ...userDetails.address,
+                pincode: e.target.value,
+              },
+            });
+          }}
+          placeholder="pincode"
+        />
+      </div>
+      <div className="flex flex-row gap-40   -col items-center justify-between">
+        <button
+          className=" mt-1 bg-gradient-to-r from-sky-300 to-sky-400 text-white py-2 px-10 rounded-full"
+          onClick={prevStep}
+        >
+          Back
+        </button>
+        <button
+          className=" center  mt-1 bg-gradient-to-r from-sky-500 to-sky-300 text-white py-2 px-32 rounded-full"
+          onClick={nextStep}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  </div>
+);
---
+++ admin/src/Auth/Signup/Step4.tsx
@@ -0,0 +1,438 @@
+export const StepFour = ({
+  nextStep,
+  prevStep,
+  libraryDetails,
+  setLibraryDetails,
+}: any) => (
+  //images  - Register 4
+  <div className="flex  flex-1 flex-col  overflow-y-auto px-10 py-6 bg-white rounded-lg">
+    {/* Form fields for step 2 */}
+
+    <div className=" flex-col w-full  mb-100 gap-20">
+      {/* Library name */}
+      <label>Library Name</label>
+      <input
+        className="w-full px-3 py-2 border border-gray-800 focus:outline-none focus:ring-1 focus:ring-blue-500"
+        type="text"
+        placeholder="Library Name"
+        value={libraryDetails.libraryName}
+        onChange={(e) => {
+          setLibraryDetails({ ...libraryDetails, libraryName: e.target.value });
+        }}
+      />
+      {/* App name */}
+      <label>App</label>
+      {/* Short description */}
+      <input
+        className="w-full px-3 py-2 border border-gray-800 focus:outline-none focus:ring-1 focus:ring-blue-500"
+        type="text"
+        placeholder="shortDescription"
+        value={libraryDetails.libraryApp.shortDescription}
+        onChange={(e) => {
+          setLibraryDetails({
+            ...libraryDetails,
+            libraryApp: {
+              ...libraryDetails.libraryApp,
+              shortDescription: e.target.value,
+            },
+          });
+        }}
+      />
+      {/* Long description */}
+      <input
+        className="w-full px-3 py-2 border border-gray-800 focus:outline-none focus:ring-1 focus:ring-blue-500"
+        type="text"
+        placeholder="LongDescription"
+        value={libraryDetails.libraryApp.LongDescription}
+        onChange={(e) => {
+          setLibraryDetails({
+            ...libraryDetails,
+            libraryApp: {
+              ...libraryDetails.libraryApp,
+              LongDescription: e.target.value,
+            },
+          });
+        }}
+      />
+      {/* Library address */}
+      <label>Library Address</label>
+      {/* Line 1 */}
+      <input
+        className="w-full px-3 py-2 border border-gray-800 focus:outline-none focus:ring-1 focus:ring-blue-500"
+        type="text"
+        placeholder="Address Line 1"
+        value={libraryDetails.libraryAddress.line1}
+        onChange={(e) => {
+          setLibraryDetails({
+            ...libraryDetails,
+            libraryAddress: {
+              ...libraryDetails.libraryAddress,
+              line1: e.target.value,
+            },
+          });
+        }}
+      />
+      {/* Line 2 */}
+      <input
+        className="w-full px-3 py-2 border border-gray-800 focus:outline-none focus:ring-1 focus:ring-blue-500"
+        type="text"
+        placeholder="Address Line 2"
+        value={libraryDetails.libraryAddress.line2}
+        onChange={(e) => {
+          setLibraryDetails({
+            ...libraryDetails,
+            libraryAddress: {
+              ...libraryDetails.libraryAddress,
+              line2: e.target.value,
+            },
+          });
+        }}
+      />
+      {/* City */}
+      <input
+        className="w-full px-3 py-2 border border-gray-800 focus:outline-none focus:ring-1 focus:ring-blue-500"
+        type="text"
+        placeholder="City"
+        value={libraryDetails.libraryAddress.city}
+        onChange={(e) => {
+          setLibraryDetails({
+            ...libraryDetails,
+            libraryAddress: {
+              ...libraryDetails.libraryAddress,
+              city: e.target.value,
+            },
+          });
+        }}
+      />
+      {/* State */}
+      <input
+        className="w-full px-3 py-2 border border-gray-800 focus:outline-none focus:ring-1 focus:ring-blue-500"
+        type="text"
+        placeholder="State"
+        value={libraryDetails.libraryAddress.state}
+        onChange={(e) => {
+          setLibraryDetails({
+            ...libraryDetails,
+            libraryAddress: {
+              ...libraryDetails.libraryAddress,
+              state: e.target.value,
+            },
+          });
+        }}
+      />
+      {/* Pincode */}
+      <input
+        className="w-full px-3 py-2 border border-gray-800 focus:outline-none focus:ring-1 focus:ring-blue-500"
+        type="text"
+        placeholder="pincode"
+        value={libraryDetails.libraryAddress.pincode}
+        onChange={(e) => {
+          setLibraryDetails({
+            ...libraryDetails,
+            libraryAddress: {
+              ...libraryDetails.libraryAddress,
+              pincode: e.target.value,
+            },
+          });
+        }}
+      />
+      {/* Legal */}
+      <label>Legal</label>
+      {/* Registration */}
+      <input
+        className="w-full px-3 py-2 border border-gray-800 focus:outline-none focus:ring-1 focus:ring-blue-500"
+        type="text"
+        placeholder="Registration"
+        value={libraryDetails.libraryLegal.registration}
+        onChange={(e) => {
+          setLibraryDetails({
+            ...libraryDetails,
+            libraryLegal: {
+              ...libraryDetails.libraryLegal,
+              registration: e.target.value,
+            },
+          });
+        }}
+      />
+      {/* GST */}
+      <div className="flex gap-5 mx-5 my-5">
+        <label>GST</label>
+        <div>
+          <input
+            type="radio"
+            name="gst"
+            value="true"
+            checked={libraryDetails.libraryLegal.gst === true}
+            onChange={(e) => {
+              setLibraryDetails({
+                ...libraryDetails,
+                libraryLegal: {
+                  ...libraryDetails.libraryLegal,
+                  gst: e.target.value === "true",
+                },
+              });
+            }}
+          />
+          <label>Yes</label>
+        </div>
+        <div>
+          <input
+            type="radio"
+            name="gst"
+            value="false"
+            checked={libraryDetails.libraryLegal.gst === false}
+            onChange={(e) => {
+              setLibraryDetails({
+                ...libraryDetails,
+                libraryLegal: {
+                  ...libraryDetails.libraryLegal,
+                  gst: e.target.value === "false",
+                },
+              });
+            }}
+          />
+          <label>No</label>
+        </div>
+      </div>
+
+      <div>
+        <div>
+          <input
+            type="file"
+            placeholder="GST certificate"
+            onChange={(e) => {
+              const file = e.target.files ? e.target.files[0] : null;
+              if (file) {
+                setLibraryDetails({
+                  ...libraryDetails,
+                  libraryLegal: {
+                    ...libraryDetails.libraryLegal,
+                    uploadGst: file,
+                  },
+                });
+              }
+            }}
+          />
+          <h1>Select files</h1>
+        </div>
+
+        {/* CIN */}
+        <div className="flex gap-5 mx-5 my-5">
+          <label>CIN</label>
+          <div>
+            <input
+              type="radio"
+              name="cin"
+              value="true"
+              checked={libraryDetails.libraryLegal.cin === true}
+              onChange={(e) => {
+                setLibraryDetails({
+                  ...libraryDetails,
+                  libraryLegal: {
+                    ...libraryDetails.libraryLegal,
+                    cin: e.target.value === "true",
+                  },
+                });
+              }}
+            />
+            <label>Yes</label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              name="cin"
+              value="false"
+              checked={libraryDetails.libraryLegal.cin === false}
+              onChange={(e) => {
+                setLibraryDetails({
+                  ...libraryDetails,
+                  libraryLegal: {
+                    ...libraryDetails.libraryLegal,
+                    cin: e.target.value === "false",
+                  },
+                });
+              }}
+            />
+            <label>No</label>
+          </div>
+        </div>
+        <input
+          type="text"
+          value={libraryDetails.libraryLegal.cin}
+          onChange={(e) => {
+            setLibraryDetails({
+              ...libraryDetails,
+              libraryLegal: {
+                ...libraryDetails.libraryLegal,
+                cin: e.target.value,
+              },
+            });
+          }}
+          placeholder="CIN"
+          className="w-full px-3 py-2 border border-gray-800 focus:outline-none focus:ring-1 focus:ring-blue-500"
+        />
+
+        {/* Upload CIN */}
+        <div>
+          <input
+            type="file"
+            placeholder="CIN certificate"
+            onChange={(e) => {
+              const file = e.target.files ? e.target.files[0] : null;
+              if (file) {
+                setLibraryDetails({
+                  ...libraryDetails,
+                  libraryLegal: {
+                    ...libraryDetails.libraryLegal,
+                    uploadCin: file,
+                  },
+                });
+              }
+            }}
+          />
+          <h1>Select files</h1>
+        </div>
+
+        {/* TAN */}
+        <div className="flex gap-5 mx-5 my-5">
+          <label>TAN</label>
+          <div>
+            <input
+              type="radio"
+              name="tan"
+              value="true"
+              checked={libraryDetails.libraryLegal.tan === true}
+              onChange={(e) => {
+                setLibraryDetails({
+                  ...libraryDetails,
+                  libraryLegal: {
+                    ...libraryDetails.libraryLegal,
+                    tan: e.target.value === "true",
+                  },
+                });
+              }}
+            />
+            <label>Yes</label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              name="tan"
+              value="false"
+              checked={libraryDetails.libraryLegal.tan === false}
+              onChange={(e) => {
+                setLibraryDetails({
+                  ...libraryDetails,
+                  libraryLegal: {
+                    ...libraryDetails.libraryLegal,
+                    tan: e.target.value === "false",
+                  },
+                });
+              }}
+            />
+            <label>No</label>
+          </div>
+        </div>
+
+        {/* Upload TAN */}
+        <div>
+          <input
+            type="file"
+            placeholder="TAN certificate"
+            onChange={(e) => {
+              const file = e.target.files ? e.target.files[0] : null;
+              if (file) {
+                setLibraryDetails({
+                  ...libraryDetails,
+                  libraryLegal: {
+                    ...libraryDetails.libraryLegal,
+                    uploadTan: file,
+                  },
+                });
+              }
+            }}
+          />
+          <h1>Select files</h1>
+        </div>
+
+        {/* Misme */}
+        <div className="flex gap-5 mx-5 my-5">
+          <label>Misme</label>
+          <div>
+            <input
+              type="radio"
+              name="misme"
+              value="true"
+              checked={libraryDetails.libraryLegal.misme === true}
+              onChange={(e) => {
+                setLibraryDetails({
+                  ...libraryDetails,
+                  libraryLegal: {
+                    ...libraryDetails.libraryLegal,
+                    misme: e.target.value === "true",
+                  },
+                });
+              }}
+            />
+            <label>Yes</label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              name="misme"
+              value="false"
+              checked={libraryDetails.libraryLegal.misme === false}
+              onChange={(e) => {
+                setLibraryDetails({
+                  ...libraryDetails,
+                  libraryLegal: {
+                    ...libraryDetails.libraryLegal,
+                    misme: e.target.value === "false",
+                  },
+                });
+              }}
+            />
+            <label>No</label>
+          </div>
+        </div>
+
+        {/* Upload Misme */}
+        <div>
+          <input
+            type="file"
+            placeholder="Misme certificate"
+            onChange={(e) => {
+              const file = e.target.files ? e.target.files[0] : null;
+              if (file) {
+                setLibraryDetails({
+                  ...libraryDetails,
+                  libraryLegal: {
+                    ...libraryDetails.libraryLegal,
+                    uploadMisme: file,
+                  },
+                });
+              }
+            }}
+          />
+          <h1>Select files</h1>
+        </div>
+
+
+      </div>
+    </div>
+    <div className="flex flex-row gap-40  mt-20   items-center justify-between">
+      <button
+        className=" mt-1 bg-gradient-to-r from-sky-300 to-sky-400 text-white py-2 px-10 rounded-full"
+        onClick={prevStep}
+      >
+        Back
+      </button>
+      <button
+        className=" center  mt-1 bg-gradient-to-r from-sky-500 to-sky-300 text-white py-2 px-32 rounded-full"
+        onClick={nextStep}
+      >
+        Next
+      </button>
+    </div>
+  </div>
+);
