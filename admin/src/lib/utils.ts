b2dd34d2bc58b8cdfde565b18c3e377f8fb8751b
Irfan shaik <414irfann@gmail.com>
2024-07-17 09:41:39 +0000

    changes

--- admin/src/lib/utils.ts
+++ admin/src/lib/utils.ts
@@ -5,7 +5,8 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export const BASEURL='https://dhy8ch-3000.csb.app'
+export const BASEURL='https://zany-umbrella-4jqg9rg6qpr37jq-3000.app.github.dev'
 
 // export const BASEURL='https://studyroom-app.onrender.com'
 // export const BASEURL='https://humble-doodle-74gj6xr7jj7fx7xq-3000.app.github.dev'
+// export const BASEURL='https://zany-umbrella-4jqg9rg6qpr37jq-3000.app.github.dev'
