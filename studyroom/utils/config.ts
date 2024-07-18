cf55d02926bfc2ab8bb849ffdc966346388f0252
Harsh kumar <mister.harshkumar@gmail.com>
2024-07-18 20:59:43 +0530

    Updated TImeSlot

--- studyroom/utils/config.ts
+++ studyroom/utils/config.ts
@@ -1,5 +1,5 @@
 
-// export const BACKEND='http://127.0.0.1:3000'
-export const BACKEND='https://studyroom-app.onrender.com'
+export const BACKEND='https://8tzyxt-3000.csb.app'
+// export const BACKEND='https://studyroom-app.onrender.com'
 // export const BACKEND='https://humble-doodle-74gj6xr7jj7fx7xq-3000.app.github.dev'
 // export const BACKEND='https://humble-doodle-74gj6xr7jj7fx7xq-3000.app.github.dev'
