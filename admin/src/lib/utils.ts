84c3d7c0ecf8ace3429adcc742719db032901cf2
Gourish Markan <142171408+GourishMarkan@users.noreply.github.com>
2024-08-02 21:27:50 +0530

    chore: Update backend URL, handle image upload, and add lazy loading for images

--- admin/src/lib/utils.ts
+++ admin/src/lib/utils.ts
@@ -1,17 +1,14 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
 // export const BASEURL='http://127.0.0.1:3000'
 
+export const BASEURL = "https://studyroom-app.onrender.com";
 
-
-// export const BASEURL='https://studyroom-app.onrender.com'
-
-
-export const BASEURL='https://musical-space-spoon-94pgrxq6rg7c74xr-3000.app.github.dev'
+// export const BASEURL='https://musical-space-spoon-94pgrxq6rg7c74xr-3000.app.github.dev'
 // export const BASEURL='https://zany-umbrella-4jqg9rg6qpr37jq-3000.app.github.dev'
 // export const BASEURL='https://studyroom-app.onrender.com'
