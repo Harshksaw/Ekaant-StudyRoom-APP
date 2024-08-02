671c6168117676521173b6f362caba4e43f934ee
Harsh kumar <mister.harshkumar@gmail.com>
2024-08-02 19:13:17 +0530

    chore: Update backend URL, handle image upload, and add lazy loading for images in welcome screen

--- studyroom/utils/config.ts
+++ studyroom/utils/config.ts
@@ -1,4 +1,4 @@
 
 // export const BACKEND='https://127.0.0.1;3000'
-export const BACKEND='https://musical-space-spoon-94pgrxq6rg7c74xr-3000.app.github.dev'
-// export const BACKEND='https://humble-doodle-74gj6xr7jj7fx7xq-3000.app.github.dev'
+// export const BACKEND='https://musical-space-spoon-94pgrxq6rg7c74xr-3000.app.github.dev'
+export const BACKEND='https://studyroom-app.onrender.com'
