4f9e08fe60b123529dd0a125fba52f8373714374
Harsh kumar <mister.harshkumar@gmail.com>
2024-10-12 22:29:54 -0700

    Refactor database connection code and update backend URL in config.ts

--- Backend/src/config/server.config.js
+++ Backend/src/config/server.config.js
@@ -1,10 +1,10 @@
 
-require('dotenv').config();
+const dotenv = require("dotenv");
+dotenv.config();
 module.exports = {
-  //   PORT: process.env.PORT ,
+
   PORT: 3000,
 
-    // "mongodb+srv://indianshahishere:OTzra2mFL0l1Ry3S@studyroom.e604xri.mongodb.net/?retryWrites=true&w=majority&appName=studyroom",
-  // LOG_DB_URL: process.env.LOG_DB_URL,
+
   NODE_ENV: process.env.NODE_ENV || "development",
 };
