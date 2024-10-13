45c99f4479a478f1cf841d90579367f4619a80b9
Harsh kumar <mister.harshkumar@gmail.com>
2024-10-12 22:25:19 -0700

    Refactor database connection code and update backend URL in config.ts

--- Backend/src/config/db.config.js
+++ Backend/src/config/db.config.js
@@ -5,13 +5,10 @@ const {  NODE_ENV } = require("./server.config");
 async function connectToDB() {
   const DB_URL = process.env.DATABASE_URI;
   try {
-    if (!DB_URL) {
-      throw new Error("DB_URL is not defined. Please check your configuration.");
-    }
 
 
 
-    await mongoose.connect(DB_URL);
+    await mongoose.connect(String(DB_URL))
     console.log(`Successfully connected to the database in ${NODE_ENV} mode`);
   } catch (error) {
     console.error("Unable to connect to the DB server ---->");
