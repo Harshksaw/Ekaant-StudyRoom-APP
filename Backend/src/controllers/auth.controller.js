0c2b5cf73d1187b380fa5aba83ee1a1338ada895
Harsh kumar <mister.harshkumar@gmail.com>
2024-10-13 20:41:46 -0700

    Refactor sendOtp function to handle missing API key for Fast2SMS

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -201,10 +201,10 @@ async function sendOtp(req, res) {
 
   const url = `https://www.fast2sms.com/dev/bulkV2?authorization=${apiKey}&route=dlt&sender_id=EKAANT&message=171779&variables_values=${otp}&flash=0&numbers=${phoneNumber}`;
   const response = await axios.get(url);
-  console.log("🚀 ~ sendOtp ~ response:", response.status);
   const otpPayload = { phoneNumber, phoneotp: otp };
   const otpBody = await phoneotp.create(otpPayload);
-  console.log("🚀 ~ sendOtp ~ otpBody:", otpBody)
+  console.log("🚀 ~ sendOtp ~ response:", apiKey, otp);
+
 
 
   if (response.status == 200) {
