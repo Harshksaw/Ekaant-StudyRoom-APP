bafea41ae17d273956ae57c91f1d7b97ee30a07b
Harsh kumar <mister.harshkumar@gmail.com>
2024-10-14 18:17:55 -0700

    Refactor sendOtp function in auth.controller.js to fix response handling

--- Backend/src/routes/v1/admin.routes.js
+++ Backend/src/routes/v1/admin.routes.js
@@ -12,7 +12,7 @@ const storage = multer.diskStorage({
     cb(null, `${Date.now()}-${file.originalname}`);
   },
 });
-const upload = multer({ storage: storage });
+const upload = multer({ storage: multer.memoryStorage() });
 
 
 AdminRouter.get("/ping", AdminController.pingAdminController);
