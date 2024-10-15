dca40074fe58109b989b54066b8ca7da0b349aa2
Ashwin Ranjan <83277976+ranjanashwin@users.noreply.github.com>
2024-10-15 17:25:18 +0530

    Update utils.ts from http to https

--- admin/src/lib/utils.ts
+++ admin/src/lib/utils.ts
@@ -5,7 +5,7 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const BASEURL='http://app.studyekaant.com:3000'
+export const BASEURL='https://app.studyekaant.com:3000'
 
 // export const BASEURL = "http://127.0.0.1:3000";
 
