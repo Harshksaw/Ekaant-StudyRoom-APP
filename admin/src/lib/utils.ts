0783044db36d9522356fb8cc34205efb45b5fd73
Ashwin Ranjan <83277976+ranjanashwin@users.noreply.github.com>
2024-10-30 22:34:45 +0530

    Update utils.ts for live

--- admin/src/lib/utils.ts
+++ admin/src/lib/utils.ts
@@ -5,9 +5,9 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-// export const BASEURL='https://app.studyekaant.com'
+export const BASEURL='https://app.studyekaant.com'
 //
-export const BASEURL = "http://127.0.0.1:3000";
+//export const BASEURL = "http://127.0.0.1:3000";
 
 // export const BASEURL='https://musical-space-spoon-94pgrxq6rg7c74xr-3000.app.github.dev'
 // export const BASEURL='https://zany-umbrella-4jqg9rg6qpr37jq-3000.app.github.dev'
