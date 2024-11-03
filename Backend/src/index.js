2968fec6a1b33fd4fcd493cddcfd8ea5d9ecf611
Ashwin Ranjan <83277976+ranjanashwin@users.noreply.github.com>
2024-11-03 20:53:22 +0530

    Update index.js for file size

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -43,7 +43,7 @@ app.use(cors(
 app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(bodyParser.text());
 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 const metrics = {
   totalCalls: 0,
   failures: 0,
