dd0e3e61d47fa98923647bb98307930096033f4e
Harsh kumar <mister.harshkumar@gmail.com>
2024-11-19 21:57:05 -0800

    Update server configuration to change PORT from 3000 to 3009

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -107,7 +107,7 @@ app.get('/health', (req, res) => {
 
 app.use(errorHandler);
 
-app.listen(process.env.PORT, async () => {
+app.listen(PORT, async () => {
   console.log(`Server started at PORT: ${PORT}`);
   await connectToDB();
   console.log("Successfully connected to db");
