0e92295c14a475568ec8bddec20b4d0fade4d8e5
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2024-12-29 20:13:30 -0800

    Remove database connection configuration and update createBooking function to use correct user connection syntax

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const { PORT } = require("./config/server.config");
 const apiRouter = require("./routes");
 const errorHandler = require("./utils/errorHandler");
-const connectToDB = require("./config/db.config");
+
 const path = require('path'); 
 require('dotenv').config({ path: path.join(__dirname, '.env') });
 require("dotenv").config();
@@ -135,6 +135,6 @@ cron.schedule('0 */3 * * *', async () => {
 });
 app.listen(PORT, async () => {
   console.log(`Server started at PORT: ${PORT}`);
-  // await connectToDB();
+
   console.log("Successfully connected to db");
 });
