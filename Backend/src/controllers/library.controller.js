7c7f13e3c3c1a1f0f99de477804995d516ac8d65
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2025-01-03 00:35:16 -0800

    Fix: update library amenities handling in EditAdminLibrary function to use structured updates

--- Backend/src/controllers/library.controller.js
+++ Backend/src/controllers/library.controller.js
@@ -685,15 +685,31 @@ const EditAdminLibrary = async (req, res) => {
       registrationFees,
     } = req.body;
 
-    const library = await prisma.library.updateMany({
+    const library = await prisma.library.update({
       where: { id: parseInt(libraryId) },
       data: {
         name,
         shortDescription,
         longDescription,
-        amenities,
+        
         address,
         registrationFees,
+        amenities: {
+          update: {
+            coldWater: amenities.coldWater,
+            wifi: amenities.wifi,
+            ac: amenities.ac,
+            locker: amenities.locker,
+            separateWashroom: amenities.separateWashroom,
+            news: amenities.news,
+            discussionArea: amenities.discussionArea,
+            lunchArea: amenities.lunchArea,
+            movingChair: amenities.movingChair,
+            floorMat: amenities.floorMat,
+            separateParking: amenities.separateParking,
+            commonParking: amenities.commonParking,
+          },
+        },
       },
     });
 
