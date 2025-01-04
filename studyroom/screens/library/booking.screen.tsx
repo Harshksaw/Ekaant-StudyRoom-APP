e619804ce3bcc8349ddab911e426cf10efe40970
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2025-01-04 10:04:13 -0800

    fix: update user ID handling in booking and checkout screens

--- studyroom/screens/library/booking.screen.tsx
+++ studyroom/screens/library/booking.screen.tsx
@@ -58,13 +58,13 @@ const BookingScreen: React.FC = () => {
   const [Loading, setLoading] = useState(true);
 
   const [libraryDetails, setLibraryDetails] = useState<any>(null);
-
   const price = bookingData.details.price;
   console.log("🚀 ~ bookingData.details:", bookingData.details.price);
   const [totalAmount, setTotalAmount] = useState(0);
+  const userSelect = useSelector((state: any) => state.user.user);
+  console.log("🚀 ~ userSelect:", JSON.parse(userSelect).data.user.id)
 
-  // const subtotal = Number((price + registrationFees).toFixed(2));
-  // const totalAmount = subtotal;
+
 
   const BookedData = {
     seat: selectedSeat,
@@ -143,11 +143,15 @@ const BookingScreen: React.FC = () => {
   // console.log("🚀 ~ selectedSeat:", selectedSeat)
 
   const PreBook = async () => {
-    const userData = await AsyncStorage.getItem("userData");
+    // const userData = await AsyncStorage.getItem("userData");
 
-    const userid = JSON.parse(userData);
+   
+    // console.log("🚀 ~ PreBook ~ userSelect:", userSelect)
 
-    const userId = userid.data?.user_id?.id;
+    // const userid = JSON.parse(userData);
+    // console.log("🚀 ~ PreBook ~ userid:", userid)
+
+    const userId = JSON.parse(userSelect)?.data.user.id;
 
     if (!userId) {
       Toast.show("user data not confirgured properly, Relogin", {
