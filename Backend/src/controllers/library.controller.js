9d71dc697ca7a359a8a90ed70355293e7def38f8
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2025-01-05 11:21:48 -0800

    fix: update coords handling in createLibrary function

--- Backend/src/controllers/library.controller.js
+++ Backend/src/controllers/library.controller.js
@@ -182,7 +182,7 @@ const createLibrary = async (req, res) => {
       longDescription,
       shortDescription,
       address,
-      coords: JSON.stringify(coords),
+      coords,
       amenities: {
         create: {
           coldWater: amenities.coldWater,
