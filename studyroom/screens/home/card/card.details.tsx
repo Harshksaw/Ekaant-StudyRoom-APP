75e8f1e5ea05fde1bd028391a7425914a159cebb
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2025-01-07 23:56:32 -0800

    refactor: update amenities handling in CardDetailScreen for improved clarity

--- studyroom/screens/home/card/card.details.tsx
+++ studyroom/screens/home/card/card.details.tsx
@@ -107,11 +107,12 @@ const CardDetailScreen: React.FC<CardDetailScreenProps> = ({}) => {
     });
   };
 
-  const { id, libraryId, ...filteredAmenities } = data.amenities || {};
-  console.log("🚀 ~ filteredAmenities:", filteredAmenities);
-
-  const trueAmenities = Object.keys(filteredAmenities).filter(
-    (key) => filteredAmenities[key]
+  
+  console.log("🚀 ~ data.amenities:", data.amenities.amenities)
+  // const { id, libraryId, ...filteredAmenities } = data.amenities || {};
+  // console.log("🚀 ~ filteredAmenities:", filteredAmenities);
+  const trueAmenities = Object.keys(data.amenities.amenities).filter(
+    (key) => data.amenities.amenities[key]
   );
   const price = data.Price || 0;
 
