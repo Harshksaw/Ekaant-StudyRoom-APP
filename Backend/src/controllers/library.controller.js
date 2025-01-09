f34cd89a2775752c103735c91b697ce35576f482
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2025-01-09 05:44:47 -0800

    refactor: update city address matching to use string_contains for improved flexibility in getAllLibrary function

--- Backend/src/controllers/library.controller.js
+++ Backend/src/controllers/library.controller.js
@@ -423,8 +423,8 @@ const getAllLibrary = async (req, res) => {
       approved: true,
       address: {
         path: ['city'],
-        equals:  city || city.toLowerCase() || city.toUpperCase() || city.charAt(0).toUpperCase() + city.slice(1), 
-        mode: 'insensitive',
+string_contains: city,
+          mode: 'insensitive',
 
       },
       },
