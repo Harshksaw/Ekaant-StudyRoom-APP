998109aadb520a241b1b6f558effddee55c14cff
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2025-01-09 09:36:00 -0800

    refactor: sort locations alphabetically by name in getApp controller

--- Backend/src/controllers/app.controller.js
+++ Backend/src/controllers/app.controller.js
@@ -64,11 +64,15 @@ async function getApp(req, res) {
 
 
     const app = await prisma.app.findFirst({ where: { id: 1 }, include:{
-      locations: true
+      locations: {
+        orderBy: {
+          location: 'asc', // Sort locations alphabetically by name
+        },
+      },
     } });
-    if (app && app.locations) {
-      app.locations.sort((a, b) => a.name.localeCompare(b.name));
-    }
+    // if (app && app.locations) {
+    //   app.locations.sort((a, b) => a.name.localeCompare(b.name));
+    // }
 
     return res.status(StatusCodes.OK).json({
       success: true,
