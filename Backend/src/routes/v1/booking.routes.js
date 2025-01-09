4c3b747dd7b35856ea0ee851c858138fc2e3f8d0
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2025-01-09 14:12:22 -0800

    feat: add hasBoughtEarlier endpoint to booking controller and update version codes

--- Backend/src/routes/v1/booking.routes.js
+++ Backend/src/routes/v1/booking.routes.js
@@ -4,8 +4,8 @@ const { BookingController  } = require('../../controllers');
 const BookingRouter = express.Router();
 
 
-
 BookingRouter.get('/ping', BookingController.pingBookingController);
+BookingRouter.post('/hasBoughtEarlier', BookingController.hasBoughtEarlier);
 BookingRouter.post('/createBooking', BookingController.createBooking);
 BookingRouter.get('/getUserBookings/:id', BookingController.getUserBookings);
 BookingRouter.post('/getBookingById', BookingController.getBookingById);
@@ -13,6 +13,8 @@ BookingRouter.post('/getBookingByLibId', BookingController.getBookingByLibId);
 BookingRouter.post('/confirm/:id', BookingController.confirmBooking);
 
 BookingRouter.post('/invoices/:bookingId', BookingController.generateInvoice);
+
+
 
 
 // BookingRouter.post('/getLibrary', BookingController.generateOtp);
