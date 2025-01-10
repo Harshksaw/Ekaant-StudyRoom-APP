3131272b659434ea8ed8173f53d7873ae52cb35e
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2025-01-09 23:41:33 -0800

    feat: implement city selection dropdown in signup step and fetch cities from API

--- admin/src/lib/utils.ts
+++ admin/src/lib/utils.ts
@@ -1,5 +1,9 @@
+import axios from "axios";
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
+
+
+
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -7,8 +11,21 @@ export function cn(...inputs: ClassValue[]) {
 
 export const BASEURL='https://api.studyekaant.com'
 //
+
+
+const fetchCities = async () => {
+  try {
+    const response = await axios.get(`${BASEURL}/api/v1/app/getApp`); // Replace with your API endpoint
+    // setCities(response.data);
+    return response.data
+  } catch (error) {
+    console.error('Error fetching cities:', error);
+  }
+};
+
 // export const BASEURL = "http://127.0.0.1:3009";
 
 // export const BASEURL='https://musical-space-spoon-94pgrxq6rg7c74xr-3000.app.github.dev'
 // export const BASEURL='https://zany-umbrella-4jqg9rg6qpr37jq-3000.app.github.dev'
 // export const BASEURL='https://studyroom-app.onrender.com'
+export { fetchCities };
