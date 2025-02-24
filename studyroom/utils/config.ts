88948544908b76c2fec094b998e84084db8b677d
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2025-02-23 17:24:04 -0800

    fix: Comment out debug log in getBookings and switch backend URL to localhost for development

--- studyroom/utils/config.ts
+++ studyroom/utils/config.ts
@@ -1,2 +1,2 @@
-export const BACKEND = "https://api.studyekaant.com";
-// export const BACKEND = "https://localhost:3009";
+// export const BACKEND = "https://api.studyekaant.com";
+export const BACKEND = "https://localhost:3009";
