0360395c8c43134f44a1cd12a8a38d6109bc7e89
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2025-02-26 09:20:40 -0800

    fix: Update docker-compose for PostgreSQL service and adjust backup command

--- Backend/src/backup/index.js
+++ Backend/src/backup/index.js
@@ -8,7 +8,7 @@ const backupDatabase = (req, res) => {
     const backupFilename = `/home/ubuntu/Backup/backup_${timestamp}.sql`;
 
     // Construct the pg_dump command
-    const pgDumpCommand = `docker exec -i ekaant-studyroom-app-db-1 pg_dump -U postgres postgres > ${backupFilename}`;
+    const pgDumpCommand = `docker-compose exec -T psql pg_dump -U postgres postgres > ${backupFilename}`; 
 
     // Execute the command
     exec(pgDumpCommand, (error, stdout, stderr) => {
